5295c2980209b3878fdab77d95583e941eb4c083
Ak14n <141566736+Ak14n@users.noreply.github.com>
2023-08-21 18:35:15 +0300

    Delete home_work.py

--- homework/home_work.py
+++
@@ -1,9 +0,0 @@
-def palindrome(word):
-    word = word.lower()
-    return word == word[::-1]
-word = input("Введите слово: ")
-result = palindrome(word)
-if result:
-    print("true")
-else:
-    print("false")
